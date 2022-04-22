1a9eabf0cbe698826bf40b7dff58a8c2ec61d873
Anya <use.anya@gmail.com>
2022-04-22 21:22:22 +0300

    Добавьте файлы проекта.

---
+++ First/First.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include "Windows.h"
+
+// Дана строка. Выполнить переворот строки(записать наоборот) на ее же месте в памяти.
+
+using namespace std;
+
+void turnOver(string& text, int& first, int& last)
+{
+    if (size(text) == 1 || size(text) == 0) return;
+    swap(text[first], text[last]);
+    first += 1;
+    last -= 1;
+    if ((first == last) || (first > last)) return;
+    else turnOver(text, first, last);
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Rus");
+    string text;
+    cout << "Введите текст: ";
+    getline(cin, text);
+    int first = 0, last = size(text) - 1;
+    turnOver(text, first, last);
+    cout << "Текст наоборот: " << text;
+    return 0;
+}
---
+++ Second/Second.cpp
@@ -0,0 +1,134 @@
+﻿#include <iostream>
+
+// Определить количество вхождений: положительных, отрицательных, нулевых значений в линейном однонаправленном списке.
+
+using namespace std;
+
+struct Node // узел списка
+{
+	int element; // данные узла
+	Node* next; // указатель на следующий узел
+	Node(int _element) : element(_element), next(nullptr) {};
+};
+
+struct List // однонаправленный список
+{
+	Node* first; // указатель на первый узел списка
+	Node* last; // указатель на последний узел списка
+	List() : first(nullptr), last(nullptr) {};
+
+	void addNode(int element) // добавление узла в список
+	{
+		Node* p = new Node(element);
+		if (first == nullptr)
+		{
+			first = p;
+			last = p;
+		}
+		else
+		{
+			Node* temp = first;
+			first = p;
+			first->next = temp;
+		}
+	}
+
+	void delNode(int element) // удаление узла по элементу
+	{
+		if (first->element == element)
+		{
+			Node* temp = first;
+			first = first->next;
+			delete temp;
+			return;
+		}
+		else if (last->element == element)
+		{
+			Node* p = first;
+			while (p->next != last)
+			{
+				p = p->next;
+			}
+			delete last;
+			p->next = nullptr;
+			last = p;
+		}
+		else
+		{
+			Node* p = first;
+			Node* nxt = p->next;
+			while (nxt->element != element)
+			{
+				p = p->next;
+				nxt = p->next;
+			}
+			p->next = nxt->next;
+			delete nxt;
+		}
+	}
+
+	void outputList() // вывод списка
+	{
+		Node* p = first;
+		while (p != nullptr)
+		{
+			cout << p->element << " ";
+			p = p->next;
+		}
+	}
+};
+
+void createList(List& L)
+{
+	cout << "Введите количество элементов в списке: ";
+	int n;
+	cin >> n;
+	cout << "Введенные вами элементы: ";
+	if (n != 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			int el;
+			cin >> el;
+			L.addNode(el);
+		}
+	}
+}
+
+int countPositive(Node* node, int k)
+{
+	if (node == nullptr) return k;
+	if (node->element > 0) k += 1;
+	node = node->next;
+	countPositive(node, k);
+}
+
+int countNegative(Node* node, int k)
+{
+	if (node == nullptr) return k;
+	if (node->element < 0) k += 1;
+	node = node->next;
+	countNegative(node, k);
+}
+
+int countZero(Node* node, int k)
+{
+	if (node == nullptr) return k;
+	if (node->element == 0) k += 1;
+	node = node->next;
+	countZero(node, k);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	List L;
+	createList(L);
+	int k = 0;
+	cout << "Количество положительных значений в списке: " << countPositive(L.first, k) << endl;
+	k = 0;
+	cout << "Количество отрицательных значений в списке: " << countNegative(L.first, k) << endl;
+	k = 0;
+	cout << "Количество нулевых значений в списке: " << countZero(L.first, k);
+	return(0);
+}
